5e4ea693345e1b519aeabb1f4f62d41c3c339446
N.Shimizu <chikoski@gmail.com>
2015-09-06 11:54:45 +0900

    LD::Formオブジェクトを実装

---
+++ lib/ld/form.rb
@@ -0,0 +1,40 @@
+module LD
+  require "uri"
+
+  class Form
+
+    def initialize(title)
+      @title = title
+      @items = []
+    end
+    
+    def url=(url)
+      url = URI.parse(url) unless url.is_a?(URI)
+      @url = url
+    end
+
+    def url(url=nil)
+      self.url=(url) if url
+      return @url 
+    end
+    
+    def title=(title)
+      @title = title
+    end
+
+    def title(title=nil)
+      self.title = title if title
+      return @title
+    end
+
+    class << self
+      def create(&block)
+        product =  self.new(nil)
+        product.instance_eval(&block)
+        return product
+      end
+    end
+
+  end
+
+end
